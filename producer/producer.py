dcc5e55c664cb15ad1426607a02ed4513201641f
anzhf <xandya@yandex.ru>
2023-10-26 17:50:37 +0300

    v1

--- producer/producer.py
+++ producer/producer.py
@@ -1,14 +1,8 @@
 # Import the 'pika' library for RabbitMQ communication.
 import pika
 
-# Import the 'os' library to access environment variables.
-import os
-
-# Retrieve the AMQP URL from the environment variables.
-amqp_url = os.environ['AMQP_URL']
-
 # Parse the AMQP URL to extract connection parameters.
-url_params = pika.URLParameters(amqp_url)
+url_params = pika.URLParameters('amqp://rabbit_mq?connection_attempts=10&retry_delay=10')
 
 # Establish a blocking connection to the RabbitMQ server.
 connection = pika.BlockingConnection(url_params)
@@ -18,14 +12,10 @@ chan = connection.channel()
 
 # Declare three queues named 'A', 'B', and 'C'.
 chan.queue_declare(queue='A')
-chan.queue_declare(queue='B')
-chan.queue_declare(queue='C')
+
 
 # Publish messages to the queues.
 chan.basic_publish(exchange='', routing_key='A', body='Hello-A!')
-chan.basic_publish(exchange='', routing_key='B', body='Hello-B!')
-chan.basic_publish(exchange='', routing_key='B', body='Hello-B2!')
-chan.basic_publish(exchange='', routing_key='C', body='Hello-C!')
 
 # Print a message to indicate that the messages have been produced.
 print("Produced the message")
